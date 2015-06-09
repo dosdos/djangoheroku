6be0d39f6b870edb27dfbfeeac7d6effc0be4d15
Dosdos <dav.santucci@gmail.com>
2015-06-09 15:31:54 +0200

    Serve robots file. Remove legacy template blocks.

--- djangoheroku/urls.py
+++ djangoheroku/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import patterns, include, url
 from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
-
+from django.views.generic import TemplateView
 
 urlpatterns = patterns(
     '',
@@ -11,6 +11,10 @@ urlpatterns = patterns(
 
     # django admin
     url(r'^admin/', include(admin.site.urls)),
+
+    # robots.txt file
+    url(r'^robots\.txt$', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
+
 )
 
 urlpatterns += i18n_patterns(
