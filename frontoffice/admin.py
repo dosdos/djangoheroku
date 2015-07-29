935f2cd1c05e0a338040a2ea57af5b5451761baa
Dosdos <dav.santucci@gmail.com>
2015-07-29 18:08:56 +0200

    Implement a custom UserProfileAdmin for UserProfile model

--- frontoffice/admin.py
+++ frontoffice/admin.py
@@ -2,4 +2,14 @@ from django.contrib import admin
 from .models import UserProfile
 
 
-admin.site.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('username',
+                    'first_name',
+                    'last_name',
+                    )
+    list_display_links = ('username', )
+    save_on_top = True
+    list_per_page = 20
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
